ae2f8a4cb3dd045e5157ee644333f9ac495c5b37
Gabriel Murillo <gabrielmurillomachicao@gmail.com>
2026-04-19 13:41:43 -0500

    Tres autograders

--- include/utec/algebra/shape.h
+++ include/utec/algebra/shape.h
@@ -1,6 +1,47 @@
 #ifndef PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
 #define PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
+#include <vector>
+namespace utec {
+    namespace tf {
+        class Shape {
 
+            std::vector<int> data;
+        public:
+            Shape() = default;
+            int operator[](int i)const {
+                return data[i];
+            };
+            friend bool operator==(const Shape& lhs, const Shape& rhs) {
+                return lhs.data == rhs.data;
+            };
+            explicit Shape (std::vector<int> data) : data(std::move(data)) {}
+            Shape(int x, int y, int z) : data{x, y, z} {
+                if (x<0 || y<0 || z<0) {
+                    throw std::invalid_argument("Invalid shape");
+                }
+            }
+            Shape(int x, int y):data{x,y} {
+                if (x<=0||y<=0) {
+                    throw std::invalid_argument("Shape parameters must be positive");
+                }
+            }
+            std::vector<int> to_vector() {
+                return data;
+            }
+            [[nodiscard]]long long rank() const {
+                return static_cast<long long>(data.size());
+            }
+            [[nodiscard]]int numel() const {
+                int respuesta=1;
+                for (int i = 0; i < rank(); ++i) {
+                    respuesta *= data[i];
+                }
+                return respuesta;
+            }
+        };
 
+    }
+}
+using utec::tf::Shape;
 
 #endif //PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
--- include/utec/algebra/tensor_backend.h
+++ include/utec/algebra/tensor_backend.h
@@ -1,10 +1,83 @@
 //
-// Created by rudri on 10/11/2020.
-//
+    // Created by rudri on 10/11/2020.
+    //
 
-#ifndef PROG3_PF_EPIC1_FEATURE1_V2026_01_TENSOR_BACKEND_H
-#define PROG3_PF_EPIC1_FEATURE1_V2026_01_TENSOR_BACKEND_H
+    #include "shape.h"
+    #ifndef PROG3_PF_EPIC1_FEATURE1_V2026_01_TENSOR_BACKEND_H
+    #define PROG3_PF_EPIC1_FEATURE1_V2026_01_TENSOR_BACKEND_H
 
+    #include <vector>
 
+    namespace utec {
+    namespace tf {
 
-#endif //PROG3_PF_EPIC1_FEATURE1_V2026_01_TENSOR_BACKEND_H
+        template<typename T>
+        class Tensor {
+        private:
+            std::vector<T> values;
+            std::vector<int> dim;
+
+        public:
+            Tensor() = default;
+            Shape shape() {
+                return Shape(dim);
+            }
+            int numel(){
+                int numel = 1;
+                for(int i = 0; i < dim.size(); i++) {
+                    numel *= dim[i];
+                }
+                return numel;
+            }
+            int rank() {
+                return dim.size();
+            }
+            static Tensor<T> zeros(Shape shape) {
+                Tensor<T> t;
+                t.dim = shape.to_vector();
+                t.values = std::vector<T>(shape.numel(), 0);
+                return t;
+            }
+
+            static Tensor<T> ones(Shape shape) {
+                Tensor<T> t;
+                t.dim = shape.to_vector();
+                t.values = std::vector<T>(shape.numel(), 1);
+                return t;
+            }
+
+            size_t numel() const {
+                return values.size();
+            }
+
+        private:
+            int flatten(const std::vector<int>& coords) const {
+                int index = 0;
+                int mult = 1;
+
+                for (int i = static_cast<int>(dim.size()) - 1; i >= 0; --i) {
+                    index += coords[i] * mult;
+                    mult *= dim[i];
+                }
+
+                return index;
+            }
+
+        public:
+            template<typename... Args>
+            T& operator()(Args... args) {
+                std::vector<int> coords = {args...};
+                return values[flatten(coords)];
+            }
+
+            template<typename... Args>
+            const T& operator()(Args... args) const {
+                std::vector<int> coords = {args...};
+                return values[flatten(coords)];
+            }
+        };
+
+    } // namespace tf
+} // namespace utec
+
+#endif
